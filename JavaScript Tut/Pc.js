82a9b65c0b6606cc343bf43d4f7151bb6de3d09a
raymond <raymonddumkwu@gmail.com>
2025-04-02 16:45:32 +0100

    Initial commit

--- JavaScript Tut/Pc.js
+++ JavaScript Tut/Pc.js
@@ -1,115 +1,318 @@
-let count= 0
-
-console.log('count')
-
-
-function increment()  {
-    console.log("The button was clicked")
-}
-
-function myAge(){
-    console.log(42)
-} 
-
-myAge()
-
-let lapscompleted = 0
-
-
-function incrementlap(){
-    lapscompleted = lapscompleted + 1
-}
-
-incrementlap()
-incrementlap()
-incrementlap()
-
-
-console.log(lapscompleted)
-
-let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
+// let saveEl = document.getElementById("save-el")
+// let countEl = document.getElementById("count-el")
 // let count = 0
 
-// function increment(){
-//     count= count + 1
-//     countEl.innerText = count
+
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
 // }
 
-// function save(){
-//     console.log(count)
+// function save() {
+//     let countStr = count + " - "
+//     saveEl.textContenttStr += countStr
+//     countEl.textContent = 0
+//     count = 0
+// }
+
+// console.log("L et's count people on the subway!")
+
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+
+// function myAge(){
+//     console.log(42)
+// } 
+
+// myAge()
+
+
+// function increment()  {
+//     console.log("The button was clicked")
+// }
+
+// function function1() {
+//     for (let i = 10; i > 3; i--){
+//         console.log(i)
+//     }
 // }
 
 
-let age = 22
-
-if (age < 21){
-    console.log("You are not of fucking age")
-
-}
-else if(age===22){
-    console.log("You are eligible")
-
-}
-else{
-    console.log("Do you!")
-} 
-
-
-let cards = [7, 3, 9]
-
-for(
-    let i = 0; i < cards.length; i++) {
-        console.log(cards[i])
-    }
+// function1()
 
 
 
-let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-} else {
-    console.log("You're out of the game! 😭")
-}
+// let lapscompleted = 0
 
-// // CASH OUT!
+
+// function incrementlap(){
+//     lapscompleted = lapscompleted + 1
+// }
+
+// incrementlap()
+// incrementlap()
+// incrementlap()
+
+// console.log(lapscompleted)
+
+
+
+//Conditional Statement//
+
+
+// let age = 22
+
+// if (age < 21){
+//     console.log("You are not of the freaking age young sir")
+
+// }
+// else if(age===22){
+//     console.log("You are eligible")
+
+// }
+// else{
+//     console.log("Do you!")
+// } 
+
+
+// let cards = [7, 3, 9]
+
+// for(
+//     let i = 0; i < cards.length; i++) {
+//         console.log(cards[i])
+    // }
 
 
 
 // let firstCard = 10
 // let secondCard = 11
-// let sum = firstCard + secondCard + 4
-let hasBlackJack = false
-let isAlive = true
+// let sum = firstCard + secondCard
+
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new card? 🙂")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! 🥳")
+// } else {
+//     console.log("You're out of the game! 😭")
+// }
 
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-    isAlive = false
-}
+
+
+
+// if (sum <= 20) {
+//     console.log("Do you want to draw a new card? 🙂")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! 🥳")
+//    
+// } else {
+//     console.log("You're out of the game! 😭")
+
+//     
+// }
 
 //    START           FINISH       STEP SIZE
-for ( let count = 1;  count < 11;  count += 1 )  {
+// for ( let count = 1;  count < 11;  count += 1 )  {
     
-    console.log(count)
+//     console.log(count)
 
+// }
+
+
+
+
+
+
+
+
+
+//Script Tag
+
+//MEthod types
+// .Push and .Pop
+//Shift and Unshift
+//""
+//.length
+// MAth. random and Math.floor
+
+
+
+
+
+//String and Numbers
+// let name = 42
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name
+// console.log(myGreeting)
+
+// let points = 4
+// let bonusPoints = "10"
+
+// let totalPoints = points + bonusPoints
+
+// console.log(4 + 5) 
+// console.log("2" + "4")
+// console.log("5" + 1)
+// console.log(100 + "100")
+
+
+
+
+//Exercise
+// Create two variables (name & greeting) that contains your name
+// and the greeting to render on the page
+// Render the welcome message using welcomeEl.innerText
+
+
+// let welcomeEl = document.getElementById("welcome-el")
+
+// let name = "Per Harald Borgen"
+// let greeting = "Welcome back "
+
+// welcomeEl.innerText = greeting + name
+
+
+//.textContent
+
+
+
+// let countEl = document.getElementById("count-el")
+// console.log(countEl)
+
+// let count = 0
+// function increment() {
+//     count += 1
+//     countEl.innerText = count
+// }
+
+
+
+
+// localStorage.setItem("age", 22)
+// console.log(sessionStorage.getItem("age"))
+
+
+
+
+// var, let, const
+// var -> global and local scope
+// let -> block scope
+// const -> block and constant
+
+// var sample = "any"
+// const pi = 22/7
+// pi = 10
+
+
+function anygf(){
+    let sample = "any outside"
+    // for(let i = 0; i < 10; i++){
+// var sample = "any inside"
+    // }
+    if(0 < 1){
+        let sample = "any inside" 
+        console.log(sample)
+    }
+    // console.log(sample)
+    
 }
+// anygf()
+
+// console.log(sample)
 
 
 
+//arithmetic operators
+//loops
+//functions a reusability of code
+
+
+//Variables
+//automatically
+// x = 5
+// y = 6
+// z = x + y
+// console.log(z)
+
+
+// let name = "John" + " " + "Doe"  // string 
+// const age = 22 // number
+// let isMarried = false // 
+
+// let fruits = ["apple", "banana", "mango"]
+// const bank = ["GTB", "Zenith", "Access"] 
+// let middleName; // undefined
+// let emptyValue = null
+
+// let Numbers = [1,2,3,]
+// Numbers.push(4) add an element to the end of the array
+// Numbers.pop() // remove the last element in the array
+// Numbers.shift() // remove the first element in the array
+//Numbers.unshift(8) // add an element to the beginning of the array
+// Numbers.length // get the length of the array
+//Numbers[1] = 5 // change the value of the first element in the array
+//Numbers.slice(0,1) // get a part of the array
+// function array(){
+//     console.log("figure", Numbers)
+// } 
+
+// array()
+
+// let a = 10
+// let b = 20
+
+// let sum = a+b
+// let difference = a-b
+// let product = a*b
+// let quotient = a/b
+
+// let result = 10/3
+// console.log(Math.floor(10/3)) = 3 //round down 3
+// console.log(Math.ceil(10/3)) = 4 //round up 4
+// console.log(Math.round(10/3)) = 3 //round to the nearest integer 3
+
+// console.log(Math.random()) // generate a random number between 0 and 1
+// console.log(Math.floor(Math.random() * 10)) // generate a random number between 0 and 9
+// console.log(Math.floor(Math.random() * 10) + 1) // generate a random number between 1 and 10
+// // console.log(Math.floor(Math.random() * 10) + 1) // generate a random
+
+
+// const person = {
+//     name: "raymond",
+//     age: 22,
+//     class: "graduate",
+//     department: "information Technology",
+//     isMarried: false,
+// }
+// console.log(person)
+
+// key value pair
+// const abbeyEmployee = [
+//     {name: "raymond",
+//     age: 22,
+//     class: "graduate",
+//     department: "information Technology",
+//     isMarried: false,
+//     },
+//     {name: "ritah",
+//         age: 22,
+//         class: "graduate",
+//         department: "information Technology",
+//         isMarried: false,
+//     },
+//     {name: "Emmannuella",
+//     age: 22,
+//     class: "graduate",
+//     department: "information Technology",
+//     isMarried: false,
+//     },
+// ]
+
+// console.log(abbeyEmployee[2].name)
 
 
 
-// console.log(lapscompleted)
---
+++ JavaScript Tut/ex.js
@@ -0,0 +1,21 @@
+// const person = {
+//     name: "raymond",
+//     age: 22,
+//     class: "graduate",
+//     department: "information Technology",
+//     isMarried: false,
+// }
+// console.log(person)
+
+
+const person = {
+    name: "Favour",
+    age: 16,
+    department: "Data Analyst",
+    isMarried: false,
+}
+console.log(person)
+
+
+
+
